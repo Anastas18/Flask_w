b8ae46a8c8edcc2aecd77c303bf894f1da16a07f
Anastas18 <evgenkucha2006@gmail.com>
2023-12-28 23:22:01 +0200

    Added session to the site

--- first_try/app.py
+++ first_try/app.py
@@ -1,22 +1,61 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, session, request, url_for
+from datetime import timedelta
 
 app = Flask(__name__)
+app.secret_key = "adolf_"
+app.permanent_session_lifetime = timedelta(days=1)
+
+session = {
+    "user": "",
+    "password":"",
+    "login": False,
+}
+
+@app.route('/1', methods=["POST", "GET"])
+def second():
+    if request.method == "POST":
+        user = request.form["nm"]
+        pas_ = request.form["pas_"]
+        session["user"] = user
+        session["password"] = pas_
+        session["login"] = True
+        
+        return redirect(url_for("firth"))
+    else:
+        return render_template('second.html')   
+
+
+def is_login():
+    if session["login"] == True:
+        return True
+    else:
+        return False
+
+def account_exist(name, pas):
+    if session["user"] == name and session["password"] == pas:
+        True
+    else:
+        False
+
 
 @app.route('/')
 def main():
     return render_template('index.html')
 
-@app.route('/1')
-def second():
-    return render_template('second.html')
 
 @app.route('/2')
 def third():
-    return render_template('index.html')
+    if is_login():
+        return render_template('index.html')
+    else:
+        return redirect(url_for("second"))
 
-@app.route('/3')
+@app.route('/main')
 def firth():
-    return render_template('index.html')
+    if is_login():
+        return render_template('main.html')
+    else:
+        return redirect(url_for("second"))
 
 
 if __name__ == "__main__":
